2ba1d24775262b28bcc058bf96908c60777ccf12
Muluken Seifu <muleseifu87@gmail.com>
2025-10-07 05:18:46 +0300

    finalize everything

--- client/src/components/UploadImage/UploadImage.jsx
+++ client/src/components/UploadImage/UploadImage.jsx
@@ -19,8 +19,8 @@ const UploadImage = ({
     cloudinaryRef.current = window.cloudinary;
     widgetRef.current = cloudinaryRef.current.createUploadWidget(
       {
-        cloudName: "dcdhklrjc",
-        uploadPreset: "vx0dyjgc",
+        cloudName: "dgrtlmkh3",
+        uploadPreset: "dgrtlmkh3",
         maxFiles: 1,
       },
       (err, result) => {
